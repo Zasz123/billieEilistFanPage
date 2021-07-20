94c2d8b54183d912ed6551c3c9e18d5eadc65707
Zasz123 <ygm12345@naver.com>
2021-07-20 21:04:48 +0900

    feat: add video change function

--- public/js/index.js
+++ public/js/index.js
@@ -13,3 +13,9 @@ window.addEventListener("scroll", () => {
       .classList.remove("header__navigation--active");
   }
 });
+
+function onChangeVideo(videoUrl) {
+  let video = document.getElementById("selected_video");
+
+  video.setAttribute("src", `https://www.youtube.com/embed/${videoUrl}`);
+}
